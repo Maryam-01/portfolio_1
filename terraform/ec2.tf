07ed79a289bfc6ed043c052308ff48ceb8709f59
Maryam-01 <maryam.axm16@gmail.com>
2026-07-10 16:10:31 +0100

    provisioning rds instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -90,3 +90,43 @@ egress {
     cidr_blocks = ["0.0.0.0/0"]
 }
 }
+
+resource "aws_security_group" "allow_postgres_traffic"{
+    name = "allow-allow_postgres_traffic"
+    description = "allow postgreSQL traffic only"
+}
+ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    security_groups = [aws_security_group.ssh_only.id]
+}
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [0.0.0.0/0"]
+}
+
+resource "aws_db_instance" "db-test1" {
+    identifier = "postgres-test2"
+    allocated_storage = 20
+    db_subnet_group_name = aws_db_subnet_group.default.name
+    engine = "postgres"
+    engine_version = "14"
+    instance_class = "db.t3.micro"
+    username = "postgres"
+    password = var.db_password
+    vpc_security_group_ids = [aws_secret_group.allow_postgres_traffic.id]
+    publicly_accessible = false
+    skip_final_snapshot = true
+}
+
+output "rds_endpoint" {
+    value = aws_db_instance.db-test1.rds_endpoint
+
+}
+
+output "rds_port" {
+    value = aws_db_instance.db-test1.port
+}
