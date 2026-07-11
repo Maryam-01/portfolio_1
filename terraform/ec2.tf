252ba236b7c30dc7225bcd642d5dc304289dd412
Maryam-01 <maryam.axm16@gmail.com>
2026-07-11 18:16:53 +0100

    updating bash script in ec2.tf to load in .env

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,50 +9,51 @@ resource "aws_instance" "example" {
 
 user_data = <<-EOF
 #!/bin/bash
+set -e
 exec > /home/ubuntu/progress.txt 2>&1
 
 REPO_URL="https://github.com/Maryam-01/portfolio_1.git"
 APP_DIR="/home/ubuntu/app"
 
-export DB_HOST="${aws_db_instance.db-test1.address}"
-export DB_PORT="${aws_db_instance.db-test1.port}"
-export DB_NAME="postgres"
-export DB_USER="postgres"
-export DB_PASSWORD="${var.db_password}"
+# ---- Database values from Terraform ----
+DB_HOST="${aws_db_instance.db-test1.address}"
+DB_PORT="${aws_db_instance.db-test1.port}"
+DB_NAME="postgres"
+DB_USER="postgres"
+DB_PASSWORD="${var.db_password}"
+JWT_SECRET="super_secret_jwt_key"
 
-export DATABASE_URL="postgresql://$${DB_USER}:$${DB_PASSWORD}@$${DB_HOST}:$${DB_PORT}/$${DB_NAME}"
+DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
+# ---- Install dependencies ----
 apt update -y
 apt install -y python3 python3-pip python3-venv git
 
-git clone $REPO_URL $APP_DIR
-cd $APP_DIR
+# ---- Clone repository ----
+git clone ${REPO_URL} ${APP_DIR}
+cd ${APP_DIR}
 
+# ---- Setup virtual environment ----
 python3 -m venv venv
 venv/bin/pip install --upgrade pip
 venv/bin/pip install -r requirements.txt
 
 # ---- Seed Database ----
 echo "Seeding database..."
-env \
-    DATABASE_URL=$${DATABASE_URL} \
-    DB_HOST=$${DB_HOST} \
-    DB_PORT=$${DB_PORT} \
-    DB_NAME=$${DB_NAME} \
-    DB_USER=$${DB_USER} \
-    DB_PASSWORD=$${DB_PASSWORD} \
-venv/bin/python main.py
 
-# ---- Start API ----
+env \
+  DATABASE_URL="${DATABASE_URL}" \
+  JWT_SECRET="${JWT_SECRET}" \
+  venv/bin/python seed.py
+
+# ---- Start FastAPI ----
 echo "Starting FastAPI..."
+
 nohup env \
-    DATABASE_URL=$${DATABASE_URL} \
-    DB_HOST=$${DB_HOST} \
-    DB_PORT=$${DB_PORT} \
-    DB_NAME=$${DB_NAME} \
-    DB_USER=$${DB_USER} \
-    DB_PASSWORD=$${DB_PASSWORD} \
-venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 &
+  DATABASE_URL="${DATABASE_URL}" \
+  JWT_SECRET="${JWT_SECRET}" \
+  venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 \
+  > /home/ubuntu/app.log 2>&1 &
 EOF
 
 
